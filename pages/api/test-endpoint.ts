1149719bc300ce8cb621652abc3940aecf0a81e8
sakulstra <lukasstrassel@googlemail.com>
2021-01-26 12:51:35 +0100

    fix: remove wrong comment

--- pages/api/test-endpoint.ts
+++ pages/api/test-endpoint.ts
@@ -2,11 +2,6 @@ import { gqlSdkV2 } from "@api/subgraph";
 import { applyCacheHeaders } from "@api/utils/responseHelpers";
 import { NextApiRequest, NextApiResponse } from "next";
 
-/**
- * This api endpoint returns the current liquidity for a specified protocol version + historical rates.
- * The api answer is cached on vercel edge CDN and refreshed
- */
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   applyCacheHeaders(res);
   const response = await gqlSdkV2.Oracle();
