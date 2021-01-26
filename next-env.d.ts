3d66311f4e9b395e6c8e3f921b84978bd4aefc59
sakulstra <lukasstrassel@googlemail.com>
2021-01-26 13:01:07 +0100

    chore: add types for .env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ETHEREUM_NETWORK: string;
+      MONGO_URI: string;
+      RPC_PROVIDER: string;
+    }
+  }
+}
